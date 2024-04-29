e7020c4da23a76b3e8ef3cdeff45c326eb9f42a4
maksi <maksimmal26@gmail.com>
2024-04-29 20:09:52 +0300

    bug fixed

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
 
     let secret_number = rand::thread_rng()
         .gen_range(1..=100);
-    priintln!("the secret number is {secret_number}");
+    println!("the secret number is {secret_number}");
 
     println!("enter the number");
 
