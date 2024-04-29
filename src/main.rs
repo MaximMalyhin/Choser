d20091b2f292170eb0f960f962b83d123bce7774
maksi <maksimmal26@gmail.com>
2024-04-29 17:04:30 +0300

    added logic for entering the number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("guess the number");
+    println!("enter the number");
+
+    let mut guess_number = String::new();
+    io::stdin()
+        .read_line(&mut guess_number)
+        .expect("something went wrong");
+
+    println!("your number: {guess_number}");
 }
