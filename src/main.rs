5dfafd3ed017c2726159f66064187426f4ce5a70
maksi <maksimmal26@gmail.com>
2024-04-30 17:35:49 +0300

    added loop

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,22 @@ fn main() {
     let secret_number = rand::thread_rng()
         .gen_range(1..=100);
 
-    println!("enter the number");
+    loop {
+        println!("enter the number");
 
-    let mut guess_string = String::new();
-    io::stdin()
-        .read_line(&mut guess_string)
-        .expect("something went wrong");
+        let mut guess_string = String::new();
+        io::stdin()
+            .read_line(&mut guess_string)
+            .expect("something went wrong");
 
-    let guess_number: u32 = guess_string.trim().parse().expect("thats not a number...");
+        let guess_number: u32 = guess_string.trim().parse().expect("thats not a number...");
 
-    println!("your number: {guess_number}");
-    
-    match guess_number.cmp(&secret_number) {
-        Ordering::Less=>println!("too small"),
-        Ordering::Greater=>println!("too big"),
-        Ordering::Equal=>println!("you won!")
+        println!("your number: {guess_number}");
+
+        match guess_number.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => println!("you won!")
+        }
     }
 }
